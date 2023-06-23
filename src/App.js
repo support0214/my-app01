79f9f7fa88d43dee0f3e4358b040b1c6ad774aab
Jiwon_Support0214 <eldnjs6453@naver.com>
2023-06-23 11:23:38 +0900

    count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const countUp = () => {
+    setNumber(number + 1);
+  };
+  const countDown = () => {
+    setNumber(number - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Count</h1>
+      <div>{number}</div>
+      <button onClick={countUp}>countUp</button>
+      <button onClick={countDown}>countDown</button>
     </div>
   );
 }
